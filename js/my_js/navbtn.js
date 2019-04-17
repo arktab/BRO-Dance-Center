c4b28d91d0ab52592aff43e3594d74305ce66fbc
arktab <tabarkevich@gmail.com>
2019-04-17 20:38:27 +0300

    add nav menu with js

--- js/my_js/navbtn.js
+++ js/my_js/navbtn.js
@@ -3,14 +3,28 @@
         };
 
         function scrollFunction() {
-            if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
-                document.getElementById("myBtn").style.display = "block";
+//            if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+//                document.getElementById("myBtn").style.display = "block";
+//            } else {
+//                document.getElementById("myBtn").style.display = "none";
+//            }
+   
+//            var actual_width = window.innerWidth;
+//            if (actual_width < 1360){
+//                document.getElementById("navbar").style.display = "block";
+//            } else {
+//                document.getElementById("navbar").style.display = "none";
+//            }
+            
+             if (document.body.scrollTop > 750 || document.documentElement.scrollTop > 750 ) {
+                document.getElementById("navbar").style.display = "block";
             } else {
-                document.getElementById("myBtn").style.display = "none";
+                document.getElementById("navbar").style.display = "none";
             }
         }
 
-        //scroll to the top 
+        //scroll to the top  
+        //scroll to the top  
         function topFunction() {
             document.body.scrollTop = 0;
             document.documentElement.scrollTop = 0;
